e0e4e2e07a7473f52a7e1b0e98e055b252912288
myrual <lilin@myrual.me>
2013-12-02 22:36:18 +0800

    add new code for place an input pin text field

--- tableview/cryptolaliaInputPin.m
+++ tableview/cryptolaliaInputPin.m
@@ -8,8 +8,8 @@
 
 #import "cryptolaliaInputPin.h"
 
-@interface cryptolaliaInputPin ()
-
+@interface cryptolaliaInputPin () <UITextFieldDelegate>
+@property UITextField *pinField;
 @end
 
 @implementation cryptolaliaInputPin
@@ -26,6 +26,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    UITextField *inputPinfield = [[UITextField alloc] initWithFrame:CGRectMake(10, 200, 300, 40)];
+    inputPinfield.borderStyle = UITextBorderStyleRoundedRect;
+    inputPinfield.placeholder = @"enter pin here";
+    inputPinfield.delegate = self;
+    inputPinfield.keyboardType = UIKeyboardTypeDecimalPad;
+    self.pinField = inputPinfield;
+    [self.view addSubview:inputPinfield];
+    
+    
+    
     // Do any additional setup after loading the view from its nib.
 }
 
